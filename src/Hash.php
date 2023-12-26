2ed747b6a7b7212416fd93e8008db977057e40f1
izhengmy <izhengmy@outlook.com>
2023-12-26 21:07:45 +0800

    适配 Hyperf 3.1

--- src/Hash.php
+++ src/Hash.php
@@ -10,7 +10,7 @@ declare(strict_types=1);
  */
 namespace HyperfExtension\Hashing;
 
-use Hyperf\Utils\ApplicationContext;
+use Hyperf\Context\ApplicationContext;
 use HyperfExtension\Hashing\Contract\DriverInterface;
 use HyperfExtension\Hashing\Contract\HashInterface;
 
